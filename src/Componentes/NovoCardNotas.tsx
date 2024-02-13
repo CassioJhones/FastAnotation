049c0d6f7b81d68347bd645d45d61042b0d379aa
CASSIO <cassio.bjhones@gmail.com>
2024-02-12 23:31:30 -0300

    formulario

--- src/Componentes/NovoCardNotas.tsx
+++ src/Componentes/NovoCardNotas.tsx
@@ -1,14 +1,26 @@
 import * as Dialog from "@radix-ui/react-dialog"
-import { formatDistanceToNow } from 'date-fns'
-import { ptBR } from 'date-fns/locale'
 import { X } from 'lucide-react'
-import { useState } from "react"
+import { ChangeEvent, FormEvent, useState } from "react"
 
 export function NovoCardNotas() {
   const [deveAparecer, SetDeveAparecer] = useState(true)
+  const [conteudo, SetConteudo] = useState('')
 
-  function usuarioComecouEditar() {
+  function usuarioAtivouEditor() {
     SetDeveAparecer(false)
+  }
+
+  function usuarioDigitando(event: ChangeEvent<HTMLTextAreaElement>) {
+    SetConteudo(event.target.value)
+    if (event.target.value === '') {
+      SetDeveAparecer(true)
+    }
+  }
+
+  function usuarioSalvouNota(event: FormEvent) {
+    // remove o comportamento padrao do botao
+    event.preventDefault()
+    console.log(conteudo)
   }
 
   return (
@@ -22,25 +34,33 @@ export function NovoCardNotas() {
 
       <Dialog.Portal>
         <Dialog.Overlay className="inset-0 fixed bg-black/50" />
-        <Dialog.Content className="overflow-hidden outline-none h-[70vh] max-w-[740px] w-full bg-slate-700 rounded-md flex flex-col fixed left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2    " >
+        <Dialog.Content className="overflow-hidden outline-none h-[70vh] max-w-[658px] w-full bg-slate-700 rounded-md flex flex-col fixed left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2   " >
 
           <Dialog.Close className="absolute right-2 top-2 bg-slate-800 p-1.5 text-slate-400 hover:bg-slate-900  hover:text-red-400"  >
-            <X className="size-5" /> 
+            <X className="size-5" />
           </Dialog.Close>
 
-          <div className="flex flex-1 flex-col gap-3 p-5">
-            <span className='text-sm font-medium text-slate-300'>
-              Adicionar Nota
-            </span>
+          <form onSubmit={usuarioSalvouNota} className="flex flex-1 flex-col">
+            <div className="flex flex-1 flex-col gap-3 p-5">
+              <span className='text-sm font-medium text-slate-300'>
+                Adicionar Nota
+              </span>
 
-            {deveAparecer?(
-            <p className='text-sm leading-6 text-slate-400'>
-              Comece <button className="font-medium text-lime-400 hover:underline"> gravando uma nota </button> em audio usando o microfone, ou se preferir <button onClick={usuarioComecouEditar} className="font-medium text-lime-400 hover:underline">  utilize apenas texto</button>
-            </p>) : (<p>editor</p>)}
-          </div>
+              {deveAparecer ? (
+                <p className='text-sm leading-6 text-slate-400'>
+                  Comece <button className="font-medium text-lime-400 hover:underline"> gravando uma nota </button> em audio usando o microfone, ou se preferir <button onClick={usuarioAtivouEditor} className="font-medium text-lime-400 hover:underline">  utilize apenas texto</button>
+                </p>)
+                :
+                (
+                  <textarea autoFocus
+                    className="resize-none text-slate-400 text-lg leading-6 bg-transparent flex-1 outline-none "
+                    onChange={usuarioDigitando} />
+                )}
+            </div>
 
-          <button type="button" className=" hover:bg-lime-600 outline-none text-lime-950 text-sm w-full bg-lime-400 py-5 text-center font-bold">
-            Salvar Nota </button>
+            <button type="submit"
+              className=" hover:bg-lime-600 outline-none text-lime-950 text-sm w-full bg-lime-400 py-5 text-center font-bold">
+              Salvar Nota </button></form>
         </Dialog.Content>
       </Dialog.Portal>
 
