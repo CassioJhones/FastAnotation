63242cfccfa53dd577f5b93942ba4bb5a6b327eb
CASSIO <cassio.bjhones@gmail.com>
2024-02-14 23:08:43 -0300

    busca e storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 import { CardNotas } from './Componentes/CardNotas'
 import { NovoCardNotas } from './Componentes/NovoCardNotas'
 import Logo from './assets/LogoNlw.svg'
@@ -10,9 +10,24 @@ interface INote {
   content: string,
 }
 
-
 export function App() {
-  const [notas, setNotas] = useState<INote[]>([])
+  const [busca, SetBusca] = useState('')
+  const [notas, setNotas] = useState<INote[]>(() => {
+    const sNotasSalvasNoStorage = localStorage.getItem('AnotacaoSalva')
+
+    if (sNotasSalvasNoStorage) {
+      return JSON.parse(sNotasSalvasNoStorage)
+      // processo inverso do stringfy usado antes
+    }
+
+    return []
+  })
+
+  function UsuarioBuscandoCard(event: ChangeEvent<HTMLInputElement>) {
+    const query = event.target.value
+
+    SetBusca(query)
+  }
 
   function onCriandoAnotacao(content: string) {
     const novaAnotacao = {
@@ -21,20 +36,32 @@ export function App() {
       content,
     }
 
-    setNotas([novaAnotacao, ...notas])
+    const ArrayNotas = [novaAnotacao, ...notas]
+    setNotas(ArrayNotas)
+
+    // localStorage nao aceita Array, por isso necessario converter o array para JSON
+    localStorage.setItem('AnotacaoSalva', JSON.stringify(ArrayNotas))
   }
+
+  const NotasFiltradas = busca !== ''
+    ? notas.filter(notas => notas.content.toLocaleLowerCase().includes(busca.toLocaleLowerCase()))
+    : notas
+
 
   return (
     <div className='mx-auto max-w-6xl my-12 space-y-6'>
       <img src={Logo} alt='text expert' />
       <form className='w-full'>
-        <input type='text' placeholder='Busque em suas notas...'
+        <input
+          onChange={UsuarioBuscandoCard}
+          type='text'
+          placeholder='Busque em suas notas...'
           className='p-2 w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500' />
       </form>
       <div className='h-px bg-slate-700' />
       <div className='grid grid-cols-3 auto-rows-[250px] gap-6'>
         <NovoCardNotas onCriandoAnotacao={onCriandoAnotacao} />
-        {notas.map(note => {
+        {NotasFiltradas.map(note => {
           return <CardNotas key={note.notaID} anotacao={note} />
         })}
       </div>
